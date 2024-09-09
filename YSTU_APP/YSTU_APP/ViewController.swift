9c071c0aadd67a14f08713a6c771df5908e26ba4
Alex Neumark <alexneumark@Alexs-Air.IGD_Rostelecom>
2024-09-09 19:31:16 +0300

    Project was created.

---
+++ YSTU_APP/YSTU_APP/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YSTU_APP
+//
+//  Created by Alex Neumark on 09.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
